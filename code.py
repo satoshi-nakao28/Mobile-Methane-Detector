0fd83753208be28ba82f5f8b2b45a5ef71060db6
Satoshi NAKAO <108894502+satoshi-nakao28@users.noreply.github.com>
2022-07-12 15:01:09 +0900

    for CircuitPython v7

--- code.py
+++ code.py
@@ -8,13 +8,11 @@ oled.init()
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-
 led.value = True
 time.sleep(2)
 led.value = False
 time.sleep(0.5)
     
-
 zc05 = busio.UART(board.GP4, board.GP5, baudrate=9600, parity='N')
 
 while True:
